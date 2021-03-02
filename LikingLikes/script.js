6f0274ca54aa7e2f93ea27443480040f8da1045b
David Bass <d-bass@outlook.com>
2021-03-02 14:16:33 -0500

    LikingLikes-Assignment

---
+++ LikingLikes/script.js
@@ -0,0 +1,57 @@
+var count = 1;
+var countElement = document.querySelector("#count");
+
+
+function add1 () {
+
+  
+    var countElement = document.querySelector("#count");
+    console.log(countElement)
+    count++;
+
+    // this allows us to do innerHTML. brand new HTML
+    countElement.innerText = "The count is " + count;
+    console.log(count);
+
+
+
+}
+var count2 = 1;
+var countElement = document.querySelector("#count2");
+
+
+function add2 () {
+
+  
+    var countElement = document.querySelector("#count2");
+    console.log(countElement)
+    count2++;
+
+    // this allows us to do innerHTML. brand new HTML
+    countElement.innerText = "The count is " + count2;
+    console.log(count);
+
+
+
+}
+
+var count3 = 1;
+var countElement = document.querySelector("#count3");
+
+
+function add3 () {
+
+  
+    var countElement = document.querySelector("#count3");
+    console.log(countElement)
+    count3++;
+
+    // this allows us to do innerHTML. brand new HTML
+    countElement.innerText = "The count is " + count3;
+    console.log(count);
+
+
+
+}
+
+
